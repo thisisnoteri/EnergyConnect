439ef37508316b0066e44afe63ac8a3dbced2d17
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-12-03 10:29:47 +0800

    Updates

--- backend/index.js
+++ backend/index.js
@@ -28,17 +28,35 @@ app.get('/', (req, res) => {
   res.send('Hello from Express.js Server!');
 });
 
-// Define a GET route to fetch data from MySQL
-app.get('/data', (req, res) => {
-  const query = 'SELECT DAY_TYPE FROM Day_Type'; // replace with your table name
+// // Define a GET route to fetch data from MySQL
+// app.get('/data', (req, res) => {
+//   const query = 'SELECT DAY_TYPE FROM Day_Type'; // replace with your table name
+//   connection.query(query, (err, results) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     } else {
+//       res.json(results); // send MySQL data as JSON
+//     }
+//   });
+// });
+
+function fetchTable(res, tableName, columnName) {
+  const query = columnName === '*' 
+    ? `SELECT * FROM ${tableName}`
+    : `SELECT ${columnName} FROM ${tableName}`;
+
   connection.query(query, (err, results) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.json(results); // send MySQL data as JSON
+      res.json(results);
     }
   });
-});
+}
+
+app.get('/daytype', (req, res) => fetchTable(res, 'Day_Type', 'DAY_TYPE'));
+app.get('/users', (req, res) => fetchTable(res, 'Users', 'USERNAME'));
+app.get('/programs', (req, res) => fetchTable(res, 'PROGRAM', '*'))
 
 // Start the server
 app.listen(port, () => {
